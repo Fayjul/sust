c6fae438b273b6953d43f57ae8c5dbd77a45c101
Fayjul Islam <fayzulislam9@gmail.com>
2022-06-15 06:29:17 +0600

    after-nav-var

---
+++ frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../../componants/NavBar/NavBar.js';
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <h1>Allahu Akbar</h1>
+    </div>
+  );
+};
+
+export default HomePage;
