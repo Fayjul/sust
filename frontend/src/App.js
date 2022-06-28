089f29cceba0a9ff3dc4c9fcf08f629a386a6f2e
Fayjul Islam <fayzulislam9@gmail.com>
2022-06-28 08:32:17 +0600

    main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import NavBar from './componants/NavBar/NavBar';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <NavBar></NavBar>
     </div>
   );
 }
